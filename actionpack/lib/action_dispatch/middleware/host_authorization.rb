6a6130f3bbcaee16e570e8dd77d421cb7e13f89b
Jonathan Hefner <jonathan@hefner.pro>
2021-02-12 15:12:16 -0600

    Reduce allocations in HostAuthorization middleware
    Benchmark:
    
    ```ruby
     # frozen_string_literal: true
    require "benchmark/memory"
    
    Benchmark.memory do |x|
      x.report("nil X-Forwarded-Host - BEFORE") do
        nil.to_s.split(/,\s?/).last.to_s.sub(/:\d+\z/, "")
      end
    
      x.report("nil X-Forwarded-Host - AFTER") do
        nil&.slice(/([^, ]+?)(?::\d+)?\z/, 1) || ""
      end
    
      x.report("non-nil X-Forwarded-Host - BEFORE") do
        "example.com:80".to_s.split(/,\s?/).last.to_s.sub(/:\d+\z/, "")
      end
    
      x.report("non-nil X-Forwarded-Host - AFTER") do
        "example.com:80"&.slice(/([^, ]+?)(?::\d+)?\z/, 1) || ""
      end
    end
    ```
    
    Results:
    
    ```
    nil X-Forwarded-Host - BEFORE
                            80.000  memsize (     0.000  retained)
                             2.000  objects (     0.000  retained)
                             1.000  strings (     0.000  retained)
    nil X-Forwarded-Host - AFTER
                             0.000  memsize (     0.000  retained)
                             0.000  objects (     0.000  retained)
                             0.000  strings (     0.000  retained)
    non-nil X-Forwarded-Host - BEFORE
                           328.000  memsize (   208.000  retained)
                             5.000  objects (     2.000  retained)
                             2.000  strings (     1.000  retained)
    non-nil X-Forwarded-Host - AFTER
                            40.000  memsize (     0.000  retained)
                             1.000  objects (     0.000  retained)
                             1.000  strings (     0.000  retained)
    ```

--- actionpack/lib/action_dispatch/middleware/host_authorization.rb
+++ actionpack/lib/action_dispatch/middleware/host_authorization.rb
@@ -103,8 +103,8 @@ module ActionDispatch
 
     private
       def authorized?(request)
-        origin_host = request.get_header("HTTP_HOST").to_s.sub(/:\d+\z/, "")
-        forwarded_host = request.x_forwarded_host.to_s.split(/,\s?/).last.to_s.sub(/:\d+\z/, "")
+        origin_host = request.get_header("HTTP_HOST")&.sub(/:\d+\z/, "") || ""
+        forwarded_host = request.x_forwarded_host&.slice(/([^, ]+?)(?::\d+)?\z/, 1) || ""
 
         @permissions.allows?(origin_host) &&
           (forwarded_host.blank? || @permissions.allows?(forwarded_host))
