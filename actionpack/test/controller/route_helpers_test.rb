291a393d75b59ee24e702e3b2c871c7a9a0a2d91
Jonathan Hefner <jonathan@hefner.pro>
2022-11-18 12:29:33 -0600

    Use memoized dup of url_helpers for reinclusion
    In the case where a controller subclasses an engine's controller that,
    in turn, subclasses a controller that includes the application's
    `url_helpers` (for example, in the "isolated engine routes and helpers
    are isolated to that engine" test in `railties/test/railties/engine_test.rb`),
    this commit avoids allocating a new module per controller:
    
      ```ruby
      ActionController::Base.include Rails.application.routes.url_helpers
      C1 = Class.new(ActiveStorage::DirectUploadsController)
      C2 = Class.new(ActiveStorage::DirectUploadsController)
    
      C1.ancestors - C2.ancestors
      # BEFORE:
      # => [C1, #<Module:0x...>]
      # AFTER:
      # => [C1]
      ```
    
    This commit also reworks the `RouteHelperIntegrationTest` test to use
    the controllers defined in `actionpack/test/abstract_unit.rb`.
    Otherwise, `extend AbstractController::Railties::RoutesHelpers.with(...)`
    happens twice -- once for `ActionController::Base` and once for
    `FakeACBase` -- which causes descendents of `FakeACBase` to include an
    extra module as they flip-flop their `_routes` definitions.  Before this
    commit, the extra module only defined a `_routes` method; with the
    change from this commit, the extra module would be the memoized dup of
    `routes.url_helpers`, which would cause the "only includes one module
    with route helpers" test to falsely fail.

--- actionpack/test/controller/route_helpers_test.rb
+++ actionpack/test/controller/route_helpers_test.rb
@@ -3,41 +3,23 @@
 require "abstract_unit"
 
 class RouteHelperIntegrationTest < ActionDispatch::IntegrationTest
-  def self.routes
-    @routes ||= ActionDispatch::Routing::RouteSet.new
-  end
-
-  class FakeACBase < ActionController::Base
-    # Normally done by app initialization to ActionController::Base
-    app = RouteHelperIntegrationTest
-    extend ::AbstractController::Railties::RoutesHelpers.with(app.routes)
-  end
-
-  class ApplicationController < FakeACBase
-  end
-
-  class FooController < ApplicationController
-  end
-
   # We define many routes in these modules after they have been included into
   # the controllers. For boot performance, it's important that we don't
   # duplicate these modules and make method cache invalidation expensive.
   # https://github.com/rails/rails/pull/37927
   test "only includes one module with route helpers" do
-    app = self.class
+    url_helpers_module = SharedTestRoutes.named_routes.url_helpers_module
+    path_helpers_module = SharedTestRoutes.named_routes.path_helpers_module
 
-    url_helpers_module = app.routes.named_routes.url_helpers_module
-    path_helpers_module = app.routes.named_routes.path_helpers_module
+    assert_operator CommentsController, :<, url_helpers_module
+    assert_operator ResourcesController, :<, url_helpers_module
+    assert_not_operator ActionController::Base, :<, url_helpers_module
 
-    assert_operator FooController, :<, url_helpers_module
-    assert_operator ApplicationController, :<, url_helpers_module
-    assert_not_operator FakeACBase, :<, url_helpers_module
+    assert_operator CommentsController, :<, path_helpers_module
+    assert_operator ResourcesController, :<, path_helpers_module
+    assert_not_operator ActionController::Base, :<, path_helpers_module
 
-    assert_operator FooController, :<, path_helpers_module
-    assert_operator ApplicationController, :<, path_helpers_module
-    assert_not_operator FakeACBase, :<, path_helpers_module
-
-    included_modules = FooController.ancestors.grep_v(Class)
+    included_modules = CommentsController.ancestors.grep_v(Class)
     included_modules -= [url_helpers_module, path_helpers_module]
 
     modules_with_routes = included_modules.select do |mod|
