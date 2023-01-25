2a2fbd69ad02bdc49f3a72eb40e369ad1fe799de
Jonathan Hefner <jonathan@hefner.pro>
2023-01-25 15:51:17 -0600

    WIP binstubs

---
+++ railties/lib/rails/commands/binstubs/binstubs_command.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "pathname"
+
+module Rails
+  module Command
+    class BinstubsCommand < Base # :nodoc:
+      self.bin = "rails"
+
+      option :pattern, type: :string, default: "ruby",
+        desc: "The "
+
+      option :interpreter, type: :string, default: "/usr/bin/env #{File.basename Thor::Util.ruby_command}",
+        desc: "The "
+
+      desc "change [paths...]",
+        "Changes binstubs to have a specific shebang, Unix line endings, and execute permission"
+      def change(*paths)
+        @shebang_pattern = /#{options[:pattern]}/
+        @shebang = "#!#{options[:interpreter]}"
+        paths = ["bin"] if paths.empty?
+
+        each_file(*paths) { |path| change_binstub(path) }
+      end
+
+      private
+        def each_file(*paths, &block)
+          paths.each do |path|
+            path = Pathname(path)
+
+            if path.file?
+              block.call(path)
+            else
+              each_file(*path.children, &block)
+            end
+          end
+        end
+
+        def change_binstub(path)
+          path.open("r+") do |file|
+            shebang = file.read(2)
+
+            if shebang == "#!"
+              shebang << file.gets.chomp!
+              shebang = @shebang if @shebang_pattern.match?(shebang)
+
+              content = file.read
+              content.delete!("\r")
+
+              file.rewind
+              file.truncate(file.write(shebang, "\n", content))
+              path.chmod(0755 & ~File.umask)
+            end
+          end
+        end
+    end
+  end
+end
